545948c1ab4897f5da29cf693fdc7cf1f71dad72
Kritiksha Gadikar <kritiksha.gadikar@nelkinda.com>
2024-10-21 16:23:45 +0530

    fix

--- AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
+++ AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.thenewsapp.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.newsprojectpractice.R
 
 class NewsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
