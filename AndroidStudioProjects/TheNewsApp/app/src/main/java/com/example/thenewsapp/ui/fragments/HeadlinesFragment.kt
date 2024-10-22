711f3e5300918f92dc59b8c8ca61ed0cff5f32ff
Kritiksha Gadikar <kritiksha.gadikar@nelkinda.com>
2024-10-22 13:02:46 +0530

    feat: setting up fun onScrolled and onScrollStateChanged

--- AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
+++ AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
@@ -1,7 +1,89 @@
 package com.example.thenewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.AbsListView
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsprojectpractice.databinding.FragmentHeadlinesBinding
+import com.example.thenewsapp.adapters.NewsAdapter
+import com.example.thenewsapp.ui.NewsViewModel
+import com.example.thenewsapp.util.Constants
 
 class HeadlinesFragment : Fragment() {
 
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
+    lateinit var binding: FragmentHeadlinesBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHeadlinesBinding.bind(view)
+
+
+    }
+
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar(){
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun showProgressBar(){
+        binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    private fun hideErrorMessage(){
+        itemHeadlinesError.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private fun showErrorMessage(message: String){
+        itemHeadlinesError.visibility = View. VISIBLE
+        errorText.text = message
+        isError = true
+    }
+
+    val scrollListener = object : RecyclerView.OnScrollListener(){
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNoError = !isError
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem =firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >=0
+            val isTotalMoreThanVisible = totalItemCount >= Constants.QUERY_PAGE_SIZE
+            val shouldPaginate = isNoError && isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate) {
+                newsViewModel.getHeadlines("us")
+                isScrolling = false
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+    }
 }
--- AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/util/Constants.kt
+++ AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "fbac1c7028cb495d9f0d9115fe01ec0c"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
