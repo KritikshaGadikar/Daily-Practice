769ea3450188f3049d22d92657834db3e49561aa
Kritiksha Gadikar <kritiksha.gadikar@nelkinda.com>
2024-10-22 11:31:34 +0530

    feat: setting up the thing in NewsActivity.kt

--- AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
+++ AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -6,9 +6,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsprojectpractice.R
+import com.example.newsprojectpractice.databinding.ActivityNewsBinding
+import com.example.thenewsapp.db.ArticleDatabase
+import com.example.thenewsapp.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +29,14 @@ class NewsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+            val newsRepository =  NewsRepository(ArticleDatabase(this))
+            val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+            newsViewModel = ViewModelProvider(this, viewModelProviderFactory). get(NewsViewModel::class.java)
+
+
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            binding.bottomNavigationView.setupWithNavController(navController)
+        }
     }
-}
