5218530d0a0f2f8b80311432b488801ed6fde298
Kritiksha Gadikar <kritiksha.gadikar@nelkinda.com>
2024-10-24 17:20:01 +0530

    feat: setting up fun FavouritesFragment not completed but

--- AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/fragments/FavouritesFragment.kt
+++ AndroidStudioProjects/TheNewsApp/app/src/main/java/com/example/thenewsapp/ui/fragments/FavouritesFragment.kt
@@ -1,7 +1,50 @@
 package com.example.thenewsapp.ui.fragments
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsprojectpractice.R
+import com.example.newsprojectpractice.databinding.FragmentFavouritesBinding
+import com.example.newsprojectpractice.databinding.FragmentHeadlinesBinding
+import com.example.thenewsapp.adapters.NewsAdapter
+import com.example.thenewsapp.ui.NewsActivity
+import com.example.thenewsapp.ui.NewsViewModel
 
 class FavouritesFragment : Fragment() {
 
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: FragmentFavouritesBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentFavouritesBinding.bind(view)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setUpFavouriteRecycler()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(R.id.action_favouritesFragment2_to_articleFragment)
+        }
+
+        val itemTouchHelperCallBack = object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP  or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+
+    }
+
+    private fun setUpFavouriteRecycler() {
+        newsAdapter = NewsAdapter()
+        binding.recyclerFavourites.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 }
